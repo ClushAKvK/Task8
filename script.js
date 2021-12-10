8a99c4dcd861d147c3310e0c224373121d8bbdb4
ClushAKvK <90634189+ClushAKvK@users.noreply.github.com>
2021-12-10 11:56:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -190,7 +190,7 @@ $(".popup-form").submit(function() {
     $(".popup-submit").prop('disabled', true);
     $.ajax({
     type: "POST",
-    url: "https://formcarry.com/s/nD1zlkZWaqi",
+    url: "https://formcarry.com/s/t6OJrHdMsQt",
     data: th.serialize(),
     })
     $popupMainWindow.fadeOut();
